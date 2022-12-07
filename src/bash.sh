6f242838adab8af3ddb3f1050468169f3a8b37b1
OldZack <31441355+OldZack@users.noreply.github.com>
2022-12-07 18:02:21 -0500

    Delete src directory

--- src/bash.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update 
-sudo apt-get install -y python3-pip libjpeg8-dev zlib1g-dev nginx 
-pip3 install setuptools 
-pip3 install wheel celery 
-pip3 install torch==1.10.0+cpu torchvision==0.11.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html 
-pip3 install requests
