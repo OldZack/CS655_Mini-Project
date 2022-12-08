4d2d63d45cba47e7dff73bf1a17682b48f02f405
OldZack <31441355+OldZack@users.noreply.github.com>
2022-12-08 00:38:27 -0500

    Update worker.sh

--- RunningScripts/worker.sh
+++ RunningScripts/worker.sh
@@ -3,5 +3,7 @@ sudo apt-get install -y python3-pip libjpeg8-dev zlib1g-dev nginx
 pip3 install setuptools 
 pip3 install wheel celery 
 pip3 install torch==1.10.0+cpu torchvision==0.11.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html 
-pip3 install requests 
-python3 worker.py
+pip3 install requests
+rm -rf CS655_Mini-Project
+git clone https://github.com/OldZack/CS655_Mini-Project.git
+python3 CS655_Mini-Project/Server/worker.py
