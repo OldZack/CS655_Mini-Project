d5953570a185470ec281f0e8f63fb98cb1ccad53
OldZack <31441355+OldZack@users.noreply.github.com>
2022-12-08 00:20:11 -0500

    Update server.sh

--- RunningScripts/server.sh
+++ RunningScripts/server.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y python3-pip
 pip3 install flask
-python3 routes.py
+rm -rf CS655_Mini-Project
+git clone https://github.com/OldZack/CS655_Mini-Project.git
+python3 CS655_Mini-Project/Server/routes.py
